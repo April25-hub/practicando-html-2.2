11bea978756fb40fa1278ddb9bc6d2093d13ac77
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-11 21:07:39 -0600

    Add files via upload

---
+++ practicando html 10/html.py
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>La <abbr title="La danza Folklorica">DANZA</abbr> se creo en año siglo XIX</p>
+
+<pLA> DANZA FOLKLORICA ES UN ARTE MUY BELLO YA QUE ES UNA DE LAS PRINCIPALES REPRECENTACIONES DE MEXICO</p>
+
+</body>
+</html>
