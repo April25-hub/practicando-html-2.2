f640fff55e9bb333f8e9a001844828a00d5ea316
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-11 20:51:32 -0600

    Add files via upload

---
+++ practicando html 9/html.py
@@ -0,0 +1,10 @@
+
+<html>
+<body>
+
+<p>Oliver es el mas feo</p>
+
+<p>WWF's goal is to: <q>Oliver es el mas feo.</q></p> <h1 style="background-color:Violet;">Oliver el mas feo</h1>
+
+</body>
+</html>
