95e9a8beb4acd93357a03483264840358247dfc8
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-11 21:15:40 -0600

    Add files via upload

---
+++ practicando html 11/html.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>En este documento ira informacion sobre mi</p>
+
+<address>
+April Arzaba Diaz<br>
+Edad:17<br>
+Año nacimiento:2007<br>
+Mes:Junio<br>
+MEXICO
+</address>
+
+</body>
+</html>
